ebf5690ceb271461c99a18e1baf66d84acb520bf
Xaereus <Xaereus@users.noreply.github.com>
2019-01-29 12:16:51 -0600

    Headpats
    Headpat command and tiny hug modifications.

--- cogs/cutecog.py
+++ cogs/cutecog.py
@@ -12,7 +12,7 @@ class CuteCog:
     @commands.command()
     async def hug(self, ctx, *, member: discord.Member = None):
         """Hug someone on the server <3"""
-        embed = discord.Embed(title = "Command: hug", color = 0xFFC0CB)
+        embed = discord.Embed(color = 0xFFC0CB)
         session = aiohttp.ClientSession()
         response = await session.get(
             'http://api.giphy.com/v1/gifs/search?q=manga+hug&api_key=NLUaerigtVW04pj4P4slXZOexvpC5VN3&limit=10')
@@ -41,6 +41,29 @@ class CuteCog:
             'http://api.giphy.com/v1/gifs/search?q=kawaii+wave&api_key=NLUaerigtVW04pj4P4slXZOexvpC5VN3&limit=1')
         data = json.loads(await response.text())
         embed.set_image(url = data['data'][0]['images']['original']['url'])
+        await session.close()
+
+        await ctx.send(embed = embed)
+
+    @commands.command(aliases = ['pat'])
+    async def headpat(self, ctx, *, member: discord.Member = None):
+        embed = discord.Embed(color = 0xFFC0CB)
+        session = aiohttp.ClientSession()
+        response = await session.get(
+            'http://api.giphy.com/v1/gifs/search?q=headpat+anime&api_key=NLUaerigtVW04pj4P4slXZOexvpC5VN3&limit=1')
+        data = json.loads(await response.text())
+        embed.set_image(url = data['data'][0]['images']['original']['url'])
+
+        if member is None:
+            embed.description = f"{ctx.message.author.mention} patted my head! <3"
+        else:
+            if member.id == ctx.message.author.id:
+                embed.description = f"I'm patting {ctx.message.author.mention}'s head!"
+            elif member.id == self.bot.user.id:
+                embed.description = f"My head was patted by {ctx.message.author.mention}! <3"
+            else:
+                embed.description = f"{member.mention} just got a head pat from me!"
+
         await session.close()
 
         await ctx.send(embed = embed)
