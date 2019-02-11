b61ea84c6041c990cf4b6029d0700c8a3a151fda
Xaereus <Xaereus@users.noreply.github.com>
2019-02-11 09:08:44 -0600

    Check in parental cog.
    Insures only guardians can use the commands.

--- cogs/mamacog.py
+++ cogs/mamacog.py
@@ -1,12 +1,14 @@
 import discord
 from discord.ext import commands
 from .utils.settings import settings
+from .utils import checks
 
 
 class MamaCog:
     def __init__(self, bot):
         self.bot = bot
 
+    @checks.is_guardian()
     @commands.command(name = 'changePlaying', aliases = ['playing'])
     async def change_playing(self, ctx, *game):
         if ctx.message.author.id in settings.guardians:
@@ -15,7 +17,7 @@ class MamaCog:
             settings.save_bot_settings()
             await self.bot.change_presence(activity = discord.Game(name = game))
         else:
-            ctx.send("No!")
+            await ctx.send("No!")
 
 
 def setup(bot):
